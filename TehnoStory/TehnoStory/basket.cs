be92802a4695af4ae9a28da1164b9a582d06f522
Frolov <72816736+FrolovYA@users.noreply.github.com>
2022-03-21 12:00:26 +0300

    Add files via upload

---
+++ TehnoStory/TehnoStory/basket.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TehnoStory
+{
+    public partial class basket : Form
+    {
+        public basket()
+        {
+            InitializeComponent();
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void Main_menu_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            TehnoStory tehnostory = new TehnoStory();
+            tehnostory.Show();
+        }
+
+        private void button_catalog_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            catalog Catalog = new catalog();
+            Catalog.Show();
+        }
+
+        private void button_physical_addresses_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            PickUpPoints picuppoints = new PickUpPoints();
+            picuppoints.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_about_us_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            AboutUs abus = new AboutUs();
+            abus.Show();
+        }
+
+        private void Button_game_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Game gmae = new Game();
+            gmae.Show();
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
